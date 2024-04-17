e33474924bd428117f5bdc10c40ca8358e7d1aac
Nikita Afonin <nekitafonin@yandex.ru>
2024-04-17 11:56:01 +0300

    Настроена точка входа.

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const {mainRoute, gamesRouter} = require('./routes')
+
 const app = express()
 const PORT = 3000
+app.use(mainRoute, gamesRouter);
 
 app.listen(PORT, ()=>{
+    console.log(`Server is running at PORT http://localhost:${PORT}`);
 })
