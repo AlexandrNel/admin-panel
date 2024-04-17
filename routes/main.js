72290c6d491eb362f81ad6dd82cb1dee85ea31f3
Nikita Afonin <nekitafonin@yandex.ru>
2024-04-17 12:34:17 +0300

    Настроен роут для отправки index.html

--- routes/main.js
+++ routes/main.js
@@ -1,7 +1,9 @@
 const mainRoute = require("express").Router();
+const fs = require('fs').promises
 
-mainRoute.get("/", (req, res) => {
-  res.status(200).send("<h1>Скоро тут будет интерфейс</h1>");
+mainRoute.get("/", async (req, res) =>{
+    const data = await fs.readFile('./public/index.html', 'utf-8')
+    res.header('Content-Type', 'text/html').send(data)
 });
 
 module.exports = mainRoute;
