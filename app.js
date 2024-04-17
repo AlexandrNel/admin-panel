2db6cd96c92dc882724c294acd9a27f7434a6510
Nikita Afonin <nekitafonin@yandex.ru>
2024-04-17 13:47:30 +0300

    Подключена библиотека bodyParser

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const path = require('path')
 const {mainRoute, gamesRouter} = require('./routes')
+const bodyParser = require('body-parser')
 
 const app = express()
 const PORT = 3000
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(mainRoute, gamesRouter);
+app.use(
+    bodyParser.json(),
+    express.static(path.join(__dirname, 'public')),
+    mainRoute, 
+    gamesRouter
+)
 
 app.listen(PORT, ()=>{
     console.log(`Server is running at PORT http://localhost:${PORT}`);
