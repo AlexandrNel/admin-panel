15b2096f33d9e75851ffff18059c2e706fb7eff9
Alex <sashanelyubov2005@gmail.com>
2024-05-13 21:51:55 +0300

    Update apiRouter.js

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1,4 +1,7 @@
+const fs = require("fs").promises;
+
 const apiRouter = require("express").Router();
+const authRouter = require("./auth");
 const categoriesRouter = require("./categories");
 const gamesRouter = require("./games");
 const usersRouter = require("./users");
@@ -6,6 +9,10 @@ const usersRouter = require("./users");
 apiRouter.use("/api", gamesRouter);
 apiRouter.use("/api", usersRouter);
 apiRouter.use("/api", categoriesRouter);
-// apiRouter.use("/api", authRouter);
+apiRouter.use("/api", authRouter);
+apiRouter.get("/admin/dashboard", async (req, res) => {
+  const data = await fs.readFile("./public/admin/dashboard.html", "utf-8");
+  res.header("Content-Type", "text/html").send(data);
+});
 
 module.exports = apiRouter;
