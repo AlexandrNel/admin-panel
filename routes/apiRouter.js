c3b71307939466fe7217b3e1eeaeb4230c597b86
Alex <sashanelyubov2005@gmail.com>
2024-05-13 21:13:41 +0300

    Новые роуты

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1,4 +1,11 @@
-const apiRouter = require("express").Router;
+const apiRouter = require("express").Router();
+const categoriesRouter = require("./categories");
 const gamesRouter = require("./games");
+const usersRouter = require("./users");
 
 apiRouter.use("/api", gamesRouter);
+apiRouter.use("/api", usersRouter);
+apiRouter.use("/api", categoriesRouter);
+// apiRouter.use("/api", authRouter);
+
+module.exports = apiRouter;
